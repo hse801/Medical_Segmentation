b0084162dcda5813579a667cf4861e8a6314475f
hse801 <sarah0.hwang@gmail.com>
2022-01-17 17:09:58 +0900

    scatter plot modify

--- analysis/percent_id_analysis.py
+++ analysis/percent_id_analysis.py
@@ -18,19 +18,20 @@ import matplotlib.pylab as pylab
 
 plot %ID correlation
 compare unet, attentionunet, proposed method
+R2 coefficient, pearson coefficient
 
 """
 
-percent_ids = pd.read_csv('./%ID_analysis_209.csv')
+percent_ids = pd.read_csv('./%ID_analysis_210_new.csv')
 # print(percent_ids)
 
 # Data filtering(less outlier)
 # percent_ids = percent_ids[0.03 > percent_ids['mask']]
 
 # pearson correlation coefficient
-unet_corr = percent_ids['mask'].corr(percent_ids['unet'], method='pearson')
-attention_corr = percent_ids['mask'].corr(percent_ids['attention'], method='pearson')
-proposed_corr = percent_ids['mask'].corr(percent_ids['proposed'], method='pearson')
+unet_pearson = percent_ids['mask'].corr(percent_ids['unet'], method='pearson')
+attention_pearson = percent_ids['mask'].corr(percent_ids['attention'], method='pearson')
+proposed_pearson = percent_ids['mask'].corr(percent_ids['proposed'], method='pearson')
 
 # R squared
 unet_r2 = r2_score(percent_ids['mask'], percent_ids['unet'])
@@ -40,9 +41,15 @@ proposed_r2 = r2_score(percent_ids['mask'], percent_ids['proposed'])
 # percent_ids = percent_ids[0.015 >percent_ids['mask']]
 # percent_ids = percent_ids[0.01 >percent_ids['proposed']]
 
-# sns.scatterplot(x='mask', y='proposed', color='blue', data=percent_ids)
-# sns.regplot(x='mask', y='proposed', ci=None, data=percent_ids)
-# plt.savefig('./proposed_test.jpg')
+sns.regplot(x='mask', y='proposed', ci=None, color='blue', marker='o', data=percent_ids)
+sns.scatterplot(x='mask', y='proposed', color='black', data=percent_ids)
+
+plt.xlabel('Mask (%ID)', fontsize=15)
+plt.ylabel('Proposed (%ID)', fontsize=15)
+z = np.polyfit(percent_ids['mask'], percent_ids['proposed'], 1)
+plt.legend([f'y = {z[0]:.4f}x + {z[1]:.4f} [R2 = {proposed_r2:.4f}]'], fontsize=13.5)
+
+plt.savefig('./0117_r2_proposed.jpg')
 
 # sns.scatterplot(x='mask', y='attention', color='blue', data=percent_ids)
 # sns.regplot(x='mask', y='attention', ci=None, data=percent_ids)
@@ -60,12 +67,13 @@ proposed_r2 = r2_score(percent_ids['mask'], percent_ids['proposed'])
 # Save plot for UNET
 # z = np.polyfit(percent_ids['mask'], percent_ids['unet'], 1)
 # p = np.poly1d(z)
-#
 # pylab.plot(percent_ids['mask'], percent_ids['unet'], 'o')
 # pylab.plot(percent_ids['mask'], p(percent_ids['mask']), 'r--')
 # plt.title("y={:.6f}x+({:.6f}) [R2={:.6f}]".format(z[0], z[1], unet_r2))
+# plt.xlabel('Mask')
+# plt.ylabel('U-Net')
 # print( f"y = {z[0]}x + {z[1]}")
-# plt.savefig('./unet_r2_209_filtered.jpg')
+# plt.savefig('./unet_r2_210_new.jpg')
 
 # Save plot for ATTENTIONUNET
 # z = np.polyfit(percent_ids['mask'], percent_ids['attention'], 1)
@@ -74,20 +82,24 @@ proposed_r2 = r2_score(percent_ids['mask'], percent_ids['proposed'])
 # pylab.plot(percent_ids['mask'], percent_ids['attention'], 'o')
 # pylab.plot(percent_ids['mask'], p(percent_ids['mask']), 'r--')
 # plt.title("y={:.6f}x+({:.6f}) [R2={:.6f}]".format(z[0], z[1], attention_r2))
+# plt.xlabel('Mask')
+# plt.ylabel('Attention U-Net')
 # print( f"y = {z[0]}x + {z[1]}")
-# plt.savefig('./attention_r2_209_filtered.jpg')
+# plt.savefig('./attention_r2_210_new.jpg')
 
 # Save plot for PROPOSED
-z = np.polyfit(percent_ids['mask'], percent_ids['proposed'], 1)
-p = np.poly1d(z)
+# z = np.polyfit(percent_ids['mask'], percent_ids['proposed'], 1)
+# p = np.poly1d(z)
+#
+# pylab.plot(percent_ids['mask'], percent_ids['proposed'], 'o')
+# pylab.plot(percent_ids['mask'], p(percent_ids['mask']), 'r--')
+# plt.title("y={:.6f}x+({:.6f}) [R2={:.6f}]".format(z[0], z[1], proposed_r2))
+# plt.xlabel('Mask')
+# plt.ylabel('Proposed')
+# print( f"y = {z[0]}x + {z[1]}")
+# plt.savefig('./proposed_r2_210_new.jpg')
 
-pylab.plot(percent_ids['mask'], percent_ids['proposed'], 'o')
-pylab.plot(percent_ids['mask'], p(percent_ids['mask']), 'r--')
-plt.title("y={:.6f}x+({:.6f}) [R2={:.6f}]".format(z[0], z[1], proposed_r2))
-print( f"y = {z[0]}x + {z[1]}")
-plt.savefig('./proposed_r2_209_filtered.jpg')
-
-print(f'unet_corr = {unet_corr:.5f}, attention_corr = {attention_corr:.5f}, proposed_corr = {proposed_corr:.5f}')
+print(f'unet_pearson = {unet_pearson:.5f}, attention_pearson = {attention_pearson:.5f}, proposed_pearson = {proposed_pearson:.5f}')
 print(f'unet_r2 = {unet_r2:.5f}, attention_r2 = {attention_r2:.5f}, proposed_r2 = {proposed_r2:.5f}')
 
 
